d8cd92632dd3df1d03d95e1e2f5f7e6f058e7a9b
Eaulmesse <theo.guerin01120@gmail.com>
2022-03-04 11:15:52 +0100

    feat: Adding some css feature

--- app.js
+++ app.js
@@ -75,3 +75,35 @@ new Typewriter(txtAnim, {})
 .typeString('<strong>, Dev Web Junior" </strong>')
 .pause(1000)
 .start()
+
+const progressBar = document.querySelector('.scrollbar');
+
+let totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener('scroll', () => {
+
+  let progress = (document.documentElement.scrollTop / totalHeight) *100;
+  progressBar.style.height = `${progress}%`;
+  progressBar.style.opacity = `${progress}%`;
+
+})
+
+const progressBarClick = document.querySelector('.clickScrollbar');
+
+progressBarClick.addEventListener('click', (e) => {
+
+  let newPageScroll = e.clientY / progressBarClick.offsetHeight * totalHeight;
+  window.scrollTo({
+    top: newPageScroll,
+    behavior: 'smooth'
+  })
+
+})
+
+let debounceResize;
+window.addEventListener("resize", () => {
+  clearTimeout(debounceResize);
+  debounceResize = setTimeout(() => {
+    totalHeight = document.body.scrollHeight - window.innerHeight;
+  }, 250);
+});
